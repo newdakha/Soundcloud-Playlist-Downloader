6edf95eddcc5f4b730d2590b8ad61cd6da363035
newdakha <bmzavalko.zmp@gmail.com>
2026-07-02 18:30:32 +0500

    Create like-downloader-dep.py

---
+++ like-downloader-dep.py
@@ -0,0 +1,16 @@
+import yt_dlp
+
+def download_likes(url):
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{'key': 'FFmpegExtractAudio', 'preferredcodec': 'mp3', 'preferredquality': '192'}],
+        'outtmpl': '%(title)s.%(ext)s',
+        'cookiesfrombrowser': ('firefox',),
+        'nooverwrites': True,     # НЕ перезаписывать, если файл уже есть
+        'ignoreerrors': True,     # Не останавливаться при ошибках
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([f"{url.rstrip('/')}/likes"])
+
+if __name__ == "__main__":
+    download_likes("https://soundcloud.com/newdakha")
